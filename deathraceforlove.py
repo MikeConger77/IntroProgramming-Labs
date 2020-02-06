c95896009c7d3874dfb71fd092e41e787fcb71aa
Michael <michael.conger1@marist.edu>
2020-02-05 20:40:31 -0500

    basic script for game

---
+++ deathraceforlove.py
@@ -0,0 +1,34 @@
+def main():
+    print('Death Race for Love')
+
+    print() # Prints a blank line
+    print("Congratualtions! Juice Wrld has just invited you to a booth session after",
+          "you posted an instagram video of you performing one of his songs.")
+    print("Score:", 100)
+    input("Press Enter to continue...")
+    print()
+
+    print(" You and Juice Wrld are perfect together. Your song is made and his record",
+          "label approaches you with a deal. You sign the deal and make $100,000 every song.")
+    print("Score:", 200)
+    input("Press Enter to continue...")
+    print()
+
+    print("Your song is released and it shoots up the charts. The record label loves the",
+          "chemistry you to have and they want you to make an album together.")
+    print("Score:", 300)
+    input("Press Enter to continue...")
+    print()
+
+    print("Everything is going great with the album but sadly Juice Wrld has passed",
+          "away. The record label is stunned but still wants a complete album so they",
+          "make you finish the album to honor his legacy.")
+    print("Score:", 400)
+    input("Press Enter to continue...")
+    print()
+
+    print("The album is finished and reaches the number one spot on the Billboard top 100",
+          "charts. Congratualtions you have won the Death Race for Love!")
+
+
+main()
