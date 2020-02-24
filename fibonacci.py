50a10cafb14eb2f4a1d278d5c0659d6f3b9b051b
Michael <michael.conger1@marist.edu>
2020-02-24 17:13:50 -0500

    Wrote an interactive Fibonacci calculator.

---
+++ fibonacci.py
@@ -0,0 +1,12 @@
+
+import math
+n=int(input("Please input a number 'n'\n"))
+def Fibonacci(n):
+    Phi=((1+5**.5)/2)
+    phi=((1-5**.5)/2)
+
+    return (((Phi)**n)-(phi)**(n))/(5**(.5))
+
+print(int(Fibonacci(n)))
+
+#citation: worked with Jadyn Kennedy
